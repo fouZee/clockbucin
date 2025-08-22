5e662247bda04a3e8acf32d61656b8a70871fff5
Fouzy <72541614+fouZee@users.noreply.github.com>
2025-08-22 21:48:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ function refreshTime() {
 }
 function getGreetText() {
   var typed = new Typed("#greet-text", {
-    strings: [greetingTextTo("Irma Sayangg ♡")], //Ganti nama disini
+    strings: [greetingTextTo("Irma Sayangg")], //Ganti nama disini
     loop: false,
     typeSpeed: 50, //Atur kecepatan ketik
     showCursor: false,
