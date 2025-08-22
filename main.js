20d16aaf0ad38b9b21ca3974baf8e711bedbc440
Fouzy <72541614+fouZee@users.noreply.github.com>
2025-08-22 22:18:09 +0700

    Update main.js

--- main.js
+++ main.js
@@ -59,11 +59,11 @@ function greetingTextTo(i, delay = 100) {
 
   var greet;
 
-  if (hrs < 12) greet = "Selamat Pagii, ^";
-  else if (hrs >= 12 && hrs <= 15) greet = "Selamat Siang, ^";
+  if (hrs < 10) greet = "Selamat Pagii, ^";
+  else if (hrs >= 11 && hrs <= 15) greet = "Selamat Siang, ^";
   else if (hrs >= 15 && hrs <= 18) greet = "Selamat Sore, ^";
-   else if (hrs >= 18 && hrs <= 21) greet = "Selamat Malam, ^";
-  else greet = "Selamat Tidurr Sayang, Have A Nice Dream, ^";
+   else if (hrs >= 18 && hrs <= 23) greet = "Selamat Malam, ^";
+  else greet = "Selamat Tidurr, Have A Nice Dream, ^";
 
   return greet + delay + i;
 }
